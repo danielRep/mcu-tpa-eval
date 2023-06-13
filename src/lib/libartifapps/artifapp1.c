aac2017ec6366fc041cd6e0d5abb5962ff1dd87b
Daniel Oliveira <drawnpoetry@gmail.com>
2023-06-13 16:43:11 +0100

    feat(artif-apps): add artificial apps to test the tpa profiler
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/lib/libartifapps/artifapp1.c
+++ src/lib/libartifapps/artifapp1.c
@@ -3,14 +3,14 @@
 #define IT 10
 #define N_INSTR 10
 
-#define REPEAT_MOV_R0(count)                        \
-    do {                                            \
-        asm volatile ("mov r0, r0");                \
-        if ((count) > 1) {                          \
-            for (int i = 1; i < (count); i++) {     \
-                asm volatile ("mov r0, r0");        \
-            }                                       \
-        }                                           \
+#define REPEAT_MOV_R0(count) \
+    do { \
+        asm volatile ("mov r0, r0"); \
+        if ((count) > 1) { \
+            for (int i = 1; i < (count); i++) { \
+                asm volatile ("mov r0, r0"); \
+            } \
+        } \
     } while (0)
 
 void initialise_benchmark (void)
@@ -21,17 +21,6 @@ void initialise_benchmark (void)
 int verify_benchmark (int res)
 {
     return 1;
-}
-
-static inline void func0(void)
-{
-    #ifdef TPA_PROF
-    tpa_sample_mbb(0);
-    #else
-    #endif
-
-    /* some code */
-    REPEAT_MOV_R0(N_INSTR);
 }
 
 static inline void func1(void)
@@ -76,23 +65,34 @@ static inline void func4(void)
 
     /* some code */
     REPEAT_MOV_R0(N_INSTR);
+}
 
-    /* callback to func2 */
-    func2();
+static inline void func5(void)
+{
+    #ifdef TPA_PROF
+    tpa_sample_mbb(5);
+    #else
+    #endif
+
+    /* some code */
+    REPEAT_MOV_R0(N_INSTR);
+
+    /* callback to func3 */
+    func3();
 }
 
 int __attribute__((noinline))
 benchmark_body(int rpt)
 {
-    func0();
     func1();
     func2();
+    func3();
 
     for(int i = 0; i < IT; i++)
     {
-        func4();
+        func5();
     }
 
-    func3();
+    func4();
     return 0;
 }
