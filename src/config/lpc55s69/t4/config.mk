fb80fcc73b904cc8b32424bf26af43deca5d265d
Daniel Oliveira <drawnpoetry@gmail.com>
2023-06-19 14:41:35 +0100

    test(lpc): add t4 test that uses the artif2
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/config/lpc55s69/t4/config.mk
@@ -0,0 +1,18 @@
+# Authors: Daniel Oliveira <daniel.oliveira@dei.uminho.pt>
+#          Sandro Pinto    <sandro.pinto@dei.uminho.pt>
+#
+# contention-eval is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+
+################################################################################
+# Configuration setup
+MULTICORE		:= n
+DEBUG			:= y
+OPTIM			:= O0
+C0_ARTIF_APP	:= artifapp2
+C0_FPM			:= n
+BUS_PRIO		:= n
+C1_INTRF_APP	:= ifi
+TPA				:= prof
+LOGGING			:= n
