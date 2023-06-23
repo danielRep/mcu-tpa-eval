b4aba525129c4c9af5787c09dfe3d10cbf6a70b4
Daniel Oliveira <drawnpoetry@gmail.com>
2023-06-23 16:21:28 +0100

    feat(tpa-cfg): add tmg data structure to single file
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/config/lpc55s69/t2/tpa_cfg.h
@@ -0,0 +1,8 @@
+#ifndef __TPA_CFG_H__
+#define __TPA_CFG_H__
+
+#define N_MBBS                              5 /* MBBs */ + 1 /* Entry Point */
+
+extern tmg_t tmg_prof[N_MBBS];
+
+#endif  /* __TPA_CFG_H__ */
---
+++ src/config/lpc55s69/t4/tpa_cfg.c
@@ -0,0 +1,11 @@
+#include "tpa.h"
+
+tmg_t tmg_prof[N_MBBS] =
+{
+    {   /* MBB 0 */
+        .t_time = 0,
+        .edges = 0,
+        .n_times = NULL,
+},
+    },
+};
