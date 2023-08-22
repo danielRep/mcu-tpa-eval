bc98f4e82a739f61de9c91520dd5ba822bee2b36
Daniel Oliveira <drawnpoetry@gmail.com>
2023-08-22 10:21:06 +0100

    ref: remove depecrated tests
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/config/lpc55s69/t4/tpa_cfg.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TPA_CFG_H__
-#define __TPA_CFG_H__
-
-#define N_MBBS                              100 /* MBBs */ + 1 /* Entry Point */
-
-static uint32_t t_n_time = 0;
-
-#endif  /* __TPA_CFG_H__ */
