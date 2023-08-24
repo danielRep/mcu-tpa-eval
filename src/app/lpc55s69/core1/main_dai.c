ebce6ae97658e9094f2ca253d49496ff90f8af99
Daniel Oliveira <drawnpoetry@gmail.com>
2023-08-24 14:38:55 +0100

    feat(core1): add new interf apps for code and data
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/lpc55s69/core1/main_dai.c
+++ src/app/lpc55s69/core1/main_dai.c
@@ -13,21 +13,17 @@
 #include "platform_config.h"
 #include "platform_base_addrs.h"
 
-#define ARRAY_SIZE 2048
+extern void data_interf();
 
 int main(void)
 {
-    static int __attribute__((used)) buffer[ARRAY_SIZE] = {1};
     /* Init platform specific hardware */
     platform_init();
     printf("Hello from core1!\n");
 
     while(1)
     {
-        for (size_t i = 0; i < ARRAY_SIZE; i++)
-        {
-            buffer[i] = i;
-        }
+        data_interf();
     }
 
     return 0;
