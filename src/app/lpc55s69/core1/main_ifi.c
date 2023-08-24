9ae1fa4f7f7536a2d911a292b9857cabb98400e0
Daniel Oliveira <drawnpoetry@gmail.com>
2023-08-24 14:38:55 +0100

    feat(core1): add new interf apps for code and data
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/lpc55s69/core1/main_ifi.c
+++ src/app/lpc55s69/core1/main_ifi.c
@@ -12,29 +12,8 @@
 #include "content_eval.h"
 #include "platform_config.h"
 #include "platform_base_addrs.h"
-#include "fsl_mailbox.h"
-#include "ipi.h"
-#include "log.h"
 
 extern void fill_cache();
-
-void Interrupt31_Handler(void)  // IPI interrupt handler
-{
-    uint32_t msg = MAILBOX_GetValue(MAILBOX, kMAILBOX_CM33_Core1);
-    MAILBOX_ClearValueBits(MAILBOX, kMAILBOX_CM33_Core1, 0xffffffff);
-
-    if(msg == CORUNNER_PAUSE)
-    {
-        LOG_PRINT("TPA_MECH: Co-runner paused.\r\n");
-        SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;    //TODO: this is probably not the best way to pause the core since it does not work if we have more interrupts
-    }
-    else    //CORUNNER_RESUME
-    {
-        LOG_PRINT("TPA_MECH: Co-runner resumed.\r\n");
-        SCB->SCR &= ~SCB_SCR_SLEEPONEXIT_Msk;
-    }
-    __DSB();
-}
 
 int main(void)
 {
@@ -44,14 +23,9 @@ int main(void)
 
     while(1)
     {
-#ifdef C1_STATS
-
-#endif
 
     fill_cache();
-#ifdef C1_STATS
 
-#endif
     //printf("Core1 running!\r\n");
     }
     return 0;
