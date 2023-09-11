a31f0aa0602f6b0efb8059d3a858256b4417f004
Daniel Oliveira <drawnpoetry@gmail.com>
2023-09-11 14:11:19 +0100

    test(stm32l4): add ctt test
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/config/stm32l476/ctt/config.h
+++ src/config/stm32l476/ctt/config.h
@@ -9,10 +9,10 @@
 /* Core0 Memory Layout */
 //CODE
 #define CORE0_ROM_START                     FLASH_MEM
-#define CORE0_ROM_LEN                       0x8000 //64KB
+#define CORE0_ROM_LEN                       SRAM1_MEM_SIZE //64KB
 //DATA
 #define CORE0_RAM_START                     SRAM1_MEM
-#define CORE0_RAM_LEN                       0x8000 //32KB
+#define CORE0_RAM_LEN                       SRAM1_MEM_SIZE //32KB
 //STACK
 #define CORE0_STACK_SIZE                    0x800
 //HEAP
