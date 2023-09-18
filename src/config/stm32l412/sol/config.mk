35c90795d82a0d0557a698c016c219c7b586bd22
Daniel Oliveira <drawnpoetry@gmail.com>
2023-09-18 14:03:15 +0100

    test(stm32l412): add sol test
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/config/stm32l412/sol/config.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Configuration setup
+# Authors: Daniel Oliveira <daniel.oliveira@dei.uminho.pt>
+#          Sandro Pinto    <sandro.pinto@dei.uminho.pt>
+#
+# contention-eval is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+
+################################################################################
+# Configuration setup
+DEBUG			:= y
+OPTIM			:= O2
+
+C0_FPM			:= y
+C0_DMA1			:= y
+C0_CLK_MAX		:= y
+C0_BENCH_APP	:= mont64
