52747912953032637510827323a8043ac9f740fc
Daniel Oliveira <drawnpoetry@gmail.com>
2023-10-03 02:19:30 +0100

    feat(cy8cproto063): add core1 wake up
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/platform/cy8cproto063/asm_defs.c
+++ src/platform/cy8cproto063/asm_defs.c
@@ -9,6 +9,7 @@
  *
  */
 #include "content_eval.h"
+#include "cyble_416045_02_device.h"
 
 #define DEFINE_OFFSET(SYMBOL, STRUCT, FIELD)  \
     asm volatile("\n-> " #SYMBOL " %0 \n" : : "i"(offsetof(STRUCT, FIELD)))
@@ -16,3 +17,8 @@
 #define DEFINE_SIZE(SYMBOL, TYPE)  \
     asm volatile("\n-> " #SYMBOL " %0 \n" : : "i"(sizeof(TYPE)))
 
+void cpuss_regs_defines() __attribute__((used));
+void cpuss_regs_defines()
+{
+    DEFINE_OFFSET(CPUSS_ID, CPUSS_Type, IDENTITY);
+}
