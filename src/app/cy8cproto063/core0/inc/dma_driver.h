158bdb280cacf2809573adb3863f0ee35725c11c
Daniel Oliveira <drawnpoetry@gmail.com>
2023-10-07 15:33:32 +0100

    feat(cy8cproto063): add dma driver
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/cy8cproto063/core0/inc/dma_driver.h
+++ src/app/cy8cproto063/core0/inc/dma_driver.h
@@ -1,12 +1,18 @@
 #ifndef __DMA_DRIVER_H__
 #define __DMA_DRIVER_H__
 
-void dma0_init(void);
+#include "content_eval.h"
+
+void dma0_init(bool print);
 void dma0_start(void);
 void dma0_ch_disable(void);
+void dma0_print_copy(void);
+void dma0_reinit(void);
 
-void dma1_init(void);
+void dma1_init(bool print);
 void dma1_start(void);
 void dma1_ch_disable(void);
+void dma1_print_copy(void);
+void dma1_reinit(void);
 
 #endif  /* __DMA_DRIVER_H__ */
