9d9b456f2a7dff5d039150f8d9719f7a8f28fff7
Daniel Oliveira <drawnpoetry@gmail.com>
2023-11-27 15:17:20 +0000

    feat(core0): add tpa changes to core0 app
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/lpc55s69/core0/objects.mk
+++ src/app/lpc55s69/core0/objects.mk
@@ -1,9 +1,9 @@
 
-ifeq ($(findstring $(TESTSUITE),sol cmn slp ctt),$(TESTSUITE))
+#ifeq ($(findstring $(TESTSUITE),sol cmn slp ctt),$(TESTSUITE))
     core0-objs-y += main_eval.o
-else ifeq ($(TESTSUITE),tpa)
-    core0-objs-y += main_tpa.o
-endif
+#else ifeq ($(TESTSUITE),tpa)
+#    core0-objs-y += main_tpa.o
+#endif
 core0-objs-y += system_LPC55S69_cm33_core0.o
 core0-objs-y += platform_init.o
 ifneq (,$(filter y,$(C0_DMA0) $(C0_DMA1)))
