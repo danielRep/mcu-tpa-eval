f3b50935f8643c118e58bcf960c4f29d5f1f3ff1
Daniel Oliveira <drawnpoetry@gmail.com>
2023-11-27 15:16:34 +0000

    test(tpa): add tpa test
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/config/lpc55s69/tpa/config.h
@@ -0,0 +1,40 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "mem_defs.h"
+
+#define N_SAMPLES                           1
+#define WARMUP_HEAT                         100
+
+/* t2:
+ * CORE0-CODE: FLASH_MEM
+ * CORE0-DATA: SRAM0
+ * CORE0-CODE: FLASH_MEM
+ * CORE0-DATA: SRAM1
+ */
+
+/* Core0 Memory Layout */
+//CODE
+#define CORE0_ROM_START                     FLASH_MEM
+#define CORE0_ROM_LEN                       0x10000     //64KB
+//DATA
+#define CORE0_RAM_START                     SRAM0
+#define CORE0_RAM_LEN                       0x10000      //64KB
+//STACK
+#define CORE0_STACK_SIZE                    0x2000
+//HEAP
+#define CORE0_HEAP_SIZE                     0x400
+
+/* Core0 Memory Layout */
+//CODE
+#define CORE1_ROM_START                     FLASH_MEM+CORE0_ROM_LEN
+#define CORE1_ROM_LEN                       0x10000     //64KB
+//DATA
+#define CORE1_RAM_START                     SRAM1
+#define CORE1_RAM_LEN                       0x10000      //64KB
+//STACK
+#define CORE1_STACK_SIZE                    0x800
+//HEAP
+#define CORE1_HEAP_SIZE                     0x400
+
+#endif  /* __CONFIG_H__ */
