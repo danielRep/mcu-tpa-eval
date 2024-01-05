f692faa81aa00f87a97b82fc1ae9071e00cb3ffa
Daniel Oliveira <drawnpoetry@gmail.com>
2024-01-05 17:12:06 +0000

    ref(lpc55s69): change integration of the utpamech
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/cy8cproto063/core0/objects.mk
+++ src/app/cy8cproto063/core0/objects.mk
@@ -1,10 +1,6 @@
 CPU		:=	cortex-m0plus
 
-ifeq ($(findstring $(TESTSUITE),sol cmn slp ctt),$(TESTSUITE))
-    core0-objs-y += main_eval.o
-else ifeq ($(TESTSUITE),tpa)
-    core0-objs-y += main_tpa.o
-endif
+core0-objs-y += main_eval.o
 core0-objs-y += system_psoc6_cm0plus.o
 core0-objs-y += platform_init.o
 ifneq (,$(filter y,$(C0_DMA0) $(C0_DMA1)))
