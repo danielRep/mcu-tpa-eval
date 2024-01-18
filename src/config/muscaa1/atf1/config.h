299fcdc0270e9785ee74191014844a70b1db11d1
Daniel Oliveira <drawnpoetry@gmail.com>
2024-01-18 13:52:16 +0000

    ref(test): support utpamech to atf1 test on muscaa1
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/config/muscaa1/atf1/config.h
+++ src/config/muscaa1/atf1/config.h
@@ -20,13 +20,16 @@
 #define CORE0_STACK_SIZE                    0x2000
 //HEAP
 #define CORE0_HEAP_SIZE                     0x400
+//TPA (RAM CODE)
+#define CORE0_TPA_START                     ESRAM0
+#define CORE0_TPA_LEN                       0x8000 //32KB
 
 /* Core0 Memory Layout */
 //CODE
 #define CORE1_ROM_START                     FLASH_MEM+CORE0_ROM_LEN
 #define CORE1_ROM_LEN                       0x10000     //64KB
 //DATA
-#define CORE1_RAM_START                     ISRAM2
+#define CORE1_RAM_START                     ISRAM3
 #define CORE1_RAM_LEN                       0x8000 //32KB
 //STACK
 #define CORE1_STACK_SIZE                    0x800
--- src/config/muscaa1/atf1/utpaconfig.h
+++ src/config/muscaa1/atf1/utpaconfig.h
@@ -3,9 +3,11 @@
 
 #include "utpa.h"
 
-#define T_N_TIME            0               /* total nominal time is calculated by the profiler */
+#define T_N_TIME            2392827         /* total nominal time is calculated by the profiler */
 #define N_MBBS              15              /* number of MBBs + entrypoint MBB*/
 #define N_SUPP_EDGES        TOTAL_HW_BKPTS  /* number of supported edges per MBB (= hw breakpoints supported by the platform) */
+#define LOG_MBB_HIT_SIZE    100             /* number of MBBs hits allowed to be logged */
+#define IRQ_IPI             7               /* IRQ number of the IPI */
 
 static uint32_t t_n_time = T_N_TIME;
 
