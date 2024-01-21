ef012c0533096997eb424cd1f501e019ff4f7ee0
Daniel Oliveira <drawnpoetry@gmail.com>
2024-01-21 17:28:42 +0000

    feat(utpa): add init function
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/app/lpc55s69/core1/main_ifi.c
+++ src/app/lpc55s69/core1/main_ifi.c
@@ -13,13 +13,24 @@
 #include "platform_config.h"
 #include "platform_base_addrs.h"
 
+#ifdef TPA_MECH
+#define CM33
+#define TOTAL_HW_BKPTS  8
+#include "utpamech.h"
+#endif
+
 extern void fill_cache();
 
 int main(void)
 {
     /* Init platform specific hardware */
     platform_init();
-    printf("Hello from core1!\r\n");
+    printf("Running ifi interf app compiled at "__TIME__"...\n");
+
+#ifdef TPA_MECH
+    /* Enable IPI interrupt */
+    utpamech_corunner_init();
+#endif
 
     while(1)
     {
