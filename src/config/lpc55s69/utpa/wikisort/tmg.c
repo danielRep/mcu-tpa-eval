c414ae212edc3e1ae9cc984590e15f3e3b370295
Daniel Oliveira <drawnpoetry@gmail.com>
2024-01-31 14:45:45 +0000

    test(lpc55s69): add different test for the wikisort benchmark
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/config/lpc55s69/utpa/wikisort/tmg.c
+++ src/config/lpc55s69/utpa/wikisort/tmg.c
@@ -14,13 +14,13 @@ mbb_t tmg[] =  // MBBs need to be declared in order
             {
                 .id = 1,
                 .edge_addr = 0x1a9a,
-                .n_time = 178,
+                .n_time = 179,
             }
         }
     },
     {   /* MBB 1 */
         .addr = 0x1a9a,
-        .t_time = 178,
+        .t_time = 179,
         .type = NORMAL,
         .edges = 1,
         .edge_mbbs =
@@ -28,13 +28,13 @@ mbb_t tmg[] =  // MBBs need to be declared in order
             {
                 .id = 2,
                 .edge_addr = 0x1aa4,
-                .n_time = 81,
+                .n_time = 84,
             }
         }
     },
     {   /* MBB 2 */
         .addr = 0x1aa4,
-        .t_time = 3832486,
+        .t_time = 3832993,
         .type = NORMAL,
         .edges = 1,
         .edge_mbbs =
@@ -42,13 +42,13 @@ mbb_t tmg[] =  // MBBs need to be declared in order
             {
                 .id = 3,
                 .edge_addr = 0x1aae,
-                .n_time = 55,
+                .n_time = 58,
             }
         }
     },
     {   /* MBB 3 */
         .addr = 0x1aae,
-        .t_time = 3832486,
+        .t_time = 3833051,
         .type = NORMAL,
         .edges = 1,
         .edge_mbbs =
@@ -56,52 +56,46 @@ mbb_t tmg[] =  // MBBs need to be declared in order
             {
                 .id = 4,
                 .edge_addr = 0x1ab6,
-                .n_time = 55,
+                .n_time = 2507,
             }
         }
     },
     {   /* MBB 4 */
         .addr = 0x1ab6,
-        .t_time = 3832541,
-        .type = LOOP_EXIT,
-        .dc_reload = 20,
-        .edges = 2,
+        .t_time = 3833208,
+        .type = NORMAL,
+        .edges = 1,
         .edge_mbbs =
         {
             {
-                .id = 3,
-                .edge_addr = 0x1aae,
-                .n_time = 0,
-            },
-            {
                 .id = 5,
                 .edge_addr = 0x1ad0,
-                .n_time = 1308439,
+                .n_time = 449232,
             }
         }
     },
     {   /* MBB 5 */
         .addr = 0x1ad0,
-        .t_time = 4048243,
-        .type = NORMAL,
+        .t_time = 4051601,
+        .type = LOOP_EXIT,
         .edges = 2,
         .edge_mbbs =
         {
             {
                 .id = 2,
                 .edge_addr = 0x1aa4,
-                .n_time = 68,
+                .n_time = 71,
             },
             {
                 .id = 6,
                 .edge_addr = 0x1ad6,
-                .n_time = 53,
+                .n_time = 56,
             }
         }
     },
     {   /* MBB 6 */
         .addr = 0x1ad6,
-        .t_time = 4048296,
+        .t_time = 4051657,
         .type = NORMAL,
         .edges = 2,
         .edge_mbbs =
@@ -114,29 +108,29 @@ mbb_t tmg[] =  // MBBs need to be declared in order
             {
                 .id = 7,
                 .edge_addr = 0x1ade,
-                .n_time = 52,
+                .n_time = 55,
             }
         }
     },
     {   /* MBB 7 */
         .addr = 0x1ade,
-        .t_time = 4048348,
+        .t_time = 4051712,
         .type = NORMAL,
         .edges = 1,
         .edge_mbbs =
         {
             {
                 .id = 8,
-                .edge_addr = 0x4000318, //utpa_prof
-                //.edge_addr = 0x4000208,    //utpa_mech
-                .n_time = 96,
+                //.edge_addr = 0x40001e4, //utpa_prof
+                .edge_addr = 0x40001fc,    //utpa_mech
+                .n_time = 99,
             }
         }
     },
     {   /* MBB 8 */
-        .addr = 0x4000318,      //utpa_prof
-        //.addr = 0x4000208,    //utpa_mech
-        .t_time = 4048444,
+        //.addr = 0x40001e4,      //utpa_prof
+        .addr = 0x40001fc,    //utpa_mech
+        .t_time = 4051811,
         .type = NORMAL,
         .edges = 0,
     }
